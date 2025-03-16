d024d7c16a905d0acca8a5ad6a196992ac3d856c
Ramatoulaye Bah <141573185+RamatoulayeBah28@users.noreply.github.com>
2025-03-16 11:29:50 -0500

    added details section to first three projects

--- script.js
+++ script.js
@@ -28,31 +28,65 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// View Details Container
+// function viewDetails(modalId, btnId) {
+//   // Get the modal
+// var modal = document.getElementById(modalId);
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById(btnId);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = modal.querySelector(".close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all buttons that trigger a modal
+  document.querySelectorAll(".project-btn[data-modal]").forEach((button) => {
+    button.addEventListener("click", function () {
+      var modalId = this.getAttribute("data-modal");
+      var modal = document.getElementById(modalId);
+      if (modal) {
+        modal.style.display = "block";
+      }
+    });
+  });
+
+  // Close modal when clicking on the close button
+  document.querySelectorAll(".modal .close").forEach((close) => {
+    close.addEventListener("click", function () {
+      this.closest(".modal").style.display = "none";
+    });
+  });
+
+  // Close modal when clicking outside of it
+  window.addEventListener("click", function (event) {
+    document.querySelectorAll(".modal").forEach((modal) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+});
+
 
 // (function ($) {
 //   var $window = $(window),
